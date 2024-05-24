8e0c99b2f688e1d2fd7dcf5646746adddce17e6a
dong <964669546@qq.com>
2024-05-24 20:58:36 +0530

    修改拦截器问题

--- eoffice-forum-service/eoffice-forum-tools/src/main/java/com/eoffice/tools/controller/ToolsController.java
+++ eoffice-forum-service/eoffice-forum-tools/src/main/java/com/eoffice/tools/controller/ToolsController.java
@@ -2,20 +2,26 @@ package com.eoffice.tools.controller;
 
 import com.eoffice.tools.feign.ChatServiceClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 @RestController
+@RequestMapping("/chat")
 public class ToolsController {
 
-    @Autowired
-    private ChatServiceClient chatServiceClient;
+
+    private final ChatServiceClient chatServiceClient;
+
+    public ToolsController(ChatServiceClient chatServiceClient) {
+        this.chatServiceClient = chatServiceClient;
+    }
 
     @GetMapping("/invokeChat3")
-    public String invokeChat3(@RequestParam("msg") String msg) {
-        ResponseEntity<String> response = chatServiceClient.chat3(msg);
-        return response.getBody();
+    public ResponseEntity<String> invokeChat3(@RequestParam("msg") String msg) {
+        return chatServiceClient.chat3(msg);
     }
 }
+
--- eoffice-forum-service/eoffice-forum-tools/src/main/java/com/eoffice/tools/feign/ChatServiceClient.java
+++ eoffice-forum-service/eoffice-forum-tools/src/main/java/com/eoffice/tools/feign/ChatServiceClient.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.ResponseEntity;
 
-@FeignClient(name = "spring-ai-chat", url = "http://localhost:28080")  // 指向聊天单体项目的URL
+@FeignClient(name = "chat-service", url = "http://localhost:8080")  // 直接使用服务的 URL
 public interface ChatServiceClient {
-
     @GetMapping("/ai/chat3")
     ResponseEntity<String> chat3(@RequestParam("msg") String msg);
 }
